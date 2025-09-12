f939a093643acc006272cf9d0a7cbe7ad5bd323a
Jorge Jimenez <jorge970102@outlook.com>
2025-09-12 02:26:09 -0500

    delete all user related files and references to user in the project remove user logic as authentication is handled by auth0

--- src/interfaces/repositories/user-repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import type { CreateUserData, UpdateUserData } from "../../types/user.types";
-
-export interface IUserRepository <TUser> {
-    findByEmail(email: string): Promise<TUser> | null;
-    save(userData: CreateUserData): Promise<TUser>;
-    update(userData: UpdateUserData): Promise<TUser>;
-}
--- src/services/auth.service.ts
+++
@@ -1,9 +0,0 @@
-import { UserRepository } from '../repositories/user.repository';
-
-export class AuthService {
-    private readonly userRepository: UserRepository;
-
-    constructor(userRepository: UserRepository) {
-        this.userRepository = userRepository;
-    }
-}
